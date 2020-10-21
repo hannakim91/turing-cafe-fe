d9a1a4da582dd07c6249c7b248d3cfb766cd1edb
Hanna Kim <hannakim91@gmail.com>
2020-10-21 11:02:51 -0600

    Add inputs for name, date, time, num guests

--- src/NewRezForm/NewRezForm.js
+++ src/NewRezForm/NewRezForm.js
@@ -4,13 +4,45 @@ class NewRezForm extends Component {
   constructor() {
     super()
     this.state = {
-
+      name: "",
+      date: "",
+      time: "",
+      number: ""
     }
   }
   render() {
     return(
       <form>
-        test
+        <input
+          placeholder="name"
+          name="name"
+          type="text"
+          onChange={this.updateValue}
+          value={this.state.name}
+        ></input>
+        <input
+          placeholder="date (MM/DD)"
+          name="date"
+          type="text"
+          onChange={this.updateValue}
+          value={this.state.date}
+        ></input>
+        <input
+          placeholder="time (MM:SS)"
+          name="time"
+          type="text"
+          onChange={this.updateValue}
+          value={this.state.time}
+        ></input>
+        <input
+          onChange={this.updateValue}
+          type="number of guests"
+          min="1"
+          max="12"
+          value={this.state.number}
+          required="required"
+        ></input>
+        
       </form>
     )
   }

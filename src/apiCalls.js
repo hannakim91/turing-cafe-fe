38b088cea4b583bfdd10d8ef55a9d2ac081fb50d
Hanna Kim <hannakim91@gmail.com>
2020-10-21 10:38:24 -0600

    Write POST request for new rez

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,15 @@ export const getReservations = async () => {
   const promise = await fetch("http://localhost:3001/api/v1/reservations")
   return await promise.json()
 }
+
+export const postNewReservation = async (data) => {
+  const promise = await fetch("http://localhost:3001/api/v1/reservations",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  })
+  return await promise.json();
+}

efcb00d84e8a757869f914caed656065c4a4f595
Hanna Kim <hannakim91@gmail.com>
2020-10-21 11:55:20 -0600

    Add render test for RezCard

--- src/RezCard/RezCard.test.js
+++ src/RezCard/RezCard.test.js
@@ -8,8 +8,18 @@ import RezCard from "./RezCard";
 describe("Reservation Card Card", () => {
   it("Should render a reservation", () => {
     render(
-      <RezCard />
+      <RezCard 
+        id={1}
+        key={1}
+        name="Namey Name"
+        date="10/10"
+        time="10:10"
+        number={5}
+      />
     )
     screen.debug()
+    expect(screen.getByText("Reservation for Namey Name:")).toBeInTheDocument();
+    expect(screen.getByText("10/10 - 10:10")).toBeInTheDocument();
+    expect(screen.getByText("Party of 5")).toBeInTheDocument();
   })
 })

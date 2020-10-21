3ed3baa942a36654e24af64ea4ec33e0cce84897
Hanna Kim <hannakim91@gmail.com>
2020-10-21 11:30:55 -0600

    Add test for initial render of NewRezForm

--- src/NewRezForm/NewRezForm.test.js
+++ src/NewRezForm/NewRezForm.test.js
@@ -3,3 +3,14 @@ import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
+
+import NewRezForm from "./NewRezForm";
+describe("New Reservation Form", () => {
+  it("Should render with inputs and a submit button", () => {
+    render(<NewRezForm />);
+
+    expect(screen.getByPlaceholderText("name")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("date (MM/DD)")).toBeInTheDocument();
+    expect(screen.getByRole("button", {name: "Add Reservation"})).toBeInTheDocument();
+  })
+})

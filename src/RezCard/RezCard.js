60da8e92d40be6eb332abb973e99fa5be8b36cba
Hanna Kim <hannakim91@gmail.com>
2020-10-21 11:22:01 -0600

    Change rezcard elements

--- src/RezCard/RezCard.js
+++ src/RezCard/RezCard.js
@@ -3,9 +3,9 @@ import React from "react";
 function RezCard({ id, name, date, time, number}) {
   return (
     <article className="rez-card" id={id}>
-      <h3>Reservation {number}:</h3>
-      <h4>{name}</h4>
-      <p>{date} - {time}</p>
+      <h3>Reservation for {name}:</h3>
+      <h4>{date} - {time}</h4>
+      <p>Party of {number}</p>
     </article>
   )
 }
